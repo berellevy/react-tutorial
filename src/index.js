68cb7e52e107cff07b0decd2a54a0c1851ef8723
berellevy <berellevy@gmail.com>
2020-11-16 21:01:17 -0500

    moved state back to board

--- src/index.js
+++ src/index.js
@@ -2,18 +2,35 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-
 class Square extends React.Component {
+
   render() {
-    return <button
-      onClick={() => {alert(this.props.value)}}
-    >{this.props.value}</button>;
+    return (
+      <button
+        className="square"
+        onClick={() => {
+          this.props.onClick()
+        }}
+      >
+        {this.props.value}
+      </button>
+    );
   }
 }
 
 class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { squares: Array(9).fill(null) };
+  }
+
   renderSquare(i) {
-    return <Square value={i} />;
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    )
   }
 
   render() {
